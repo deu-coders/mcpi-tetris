952ff167d375a5ec586878e2b289e610a5fae71f
solo5star <solo_5@naver.com>
2021-12-07 20:40:57 +0900

    music.py: do not run music twice

--- music.py
+++ music.py
@@ -4,11 +4,12 @@ import subprocess
 def play_music() -> bool:
     play_music_successed = False
 
-    for python_binary in ['python', 'python3']:
+    for python_binary in ['python3', 'python']:
         try:
-            subprocess.Popen(['python', 'play.py', 'music'])
+            subprocess.Popen([python_binary, 'play.py', 'music'])
             print(f'python binary found: {python_binary}')
             play_music_successed = True
+            break
         except FileNotFoundError:
             print(f'python binary not found: {python_binary}')
 
