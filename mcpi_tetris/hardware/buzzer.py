d1d9c02cfdb193b4272f96a395074a9087f58bb1
solo5star <solo_5@naver.com>
2021-12-07 20:42:05 +0900

    buzzer.py: split play music function to two functions: play_tetris_bgm and play_tetris_bgm_loop

--- mcpi_tetris/hardware/buzzer.py
+++ mcpi_tetris/hardware/buzzer.py
@@ -23,20 +23,27 @@ class Buzzer(Hardware):
             print('Cannot play music.')
             return
 
+        for melody in constants.BUZZER_MELODIES:
+            if (melody[1] > 0) :
+                noteDuration = constants.BUZZER_WHOLENOTE / melody[1]
+            else:
+                noteDuration = constants.BUZZER_WHOLENOTE / abs(melody[1])
+                noteDuration *= 1.5
+
+            note = melody[0]
+            actualspeed = noteDuration
+            self.buzzer_pwm.ChangeFrequency(constants.BUZZER_FREQUENCIES[note])
+            time.sleep(actualspeed)
+            self.buzzer_pwm.ChangeFrequency(10)
+
+    def play_tetris_bgm_loop(self):
+        if not self.assert_hardware_enabled():
+            print('Cannot play music.')
+            return
+
         while True:
             try:
-                for melody in constants.BUZZER_MELODIES:
-                    if (melody[1] > 0) :
-                        noteDuration = constants.BUZZER_WHOLENOTE / melody[1]
-                    else:
-                        noteDuration = constants.BUZZER_WHOLENOTE / abs(melody[1])
-                        noteDuration *= 1.5
-
-                    note = melody[0]
-                    actualspeed = noteDuration
-                    self.buzzer_pwm.ChangeFrequency(constants.BUZZER_FREQUENCIES[note])
-                    time.sleep(actualspeed)
-                    self.buzzer_pwm.ChangeFrequency(10)
+                self.play_tetris_bgm()
 
             except KeyboardInterrupt:
                 print('Stop playing music ...')
